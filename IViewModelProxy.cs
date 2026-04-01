267dcd649747507ce350d57fe4d890cc4c0ad9a6
bruce <bruce@mellows.tv>
2026-04-01 19:46:32 +1100

    No need for ViewModelProxy to conform to INotifyPropertyChanging or INotifyPropertyChanged

--- IViewModelProxy.cs
+++ IViewModelProxy.cs
@@ -26,10 +26,43 @@ namespace BruceMellows.MVVM.ViewModel.Proxy;
 
 public interface IViewModelProxy<TViewModel> where TViewModel : class
 {
+	/// <summary>
+	/// This is called when a property change is begun, irrespective of whether the property value is actually changed or not.<br/>
+	/// This can be used to perform some actions before the property value is changed, such as validation or logging.
+	/// </summary>
+	/// <param name="key"></param>
+	/// <param name="viewModel"></param>
+	void PropertyChangeBegin(string key, TViewModel viewModel);
+
+	/// <summary>
+	/// This is called if a property change is actually made, immediately after the change has been made.
+	/// </summary>
+	/// <param name="key"></param>
+	/// <param name="viewModel"></param>
+	void PropertyChanged(string key, TViewModel viewModel);
+
+	/// <summary>
+	/// This is called when a property change has ended, irrespective of whether the property value is actually changed or not.<br/>
+	/// This can be used to perform some actions after the property value is changed, such as updating the UI or triggering other changes.
+	/// </summary>
+	/// <param name="key"></param>
+	/// <param name="viewModel"></param>
+	void PropertyChangeEnd(string key, TViewModel viewModel);
+
+	/// <summary>
+	/// Return the (nullable) value for the supplied key.<br/>
+	/// If the key is not found, the getDefaultValue function is called to get a default value, which is then stored and returned.<br/>
+	/// This allows for lazy initialization of properties.
+	/// </summary>
+	/// <param name="key"></param>
+	/// <param name="getDefaultValue"></param>
+	/// <returns></returns>
 	object? GetValue(string key, Func<object> getDefaultValue);
 
 	/// <summary>
-	/// Set value.
+	/// Set the value for the supplied key.<br/>
+	/// If the supplied value is not the same as the existing value, the PropertyChanged method is called.<br/>
+	/// The PropertyChangeBegin and PropertyChangeEnd methods are called regardless of whether the value is actually changed or not, allowing for actions to be performed before and after the change.<br/>
 	/// </summary>
 	bool SetValue(string key, object? value);
 
